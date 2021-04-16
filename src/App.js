3d5dfbf5b14fb1f4d9627e258e00e5684c5a0bee
liangyuW <liangyu@live.ca>
2021-04-16 13:56:17 -0400

    update e-commerce app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import {Switch, Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.comonent.jsx
+++ src/components/directory/directory.comonent.jsx
@@ -1,5 +1,5 @@
 
-import { render } from '@testing-library/react';
+
 import React from 'react';
 
 import MenuItem from '../menu-item/menu-item.component';
@@ -16,33 +16,33 @@ class Directory extends React.Component {
                   title: 'Appetizers',
                   imageUrl: 'https://live.staticflickr.com/95/276455203_900469b2ae.jpg',
                   id: 1,
-                  linkUrl: 'shop/appetizers'
+                  linkUrl: 'appetizers'
                 },
                 {
                   title: 'Fried Rice',
                   imageUrl: 'https://live.staticflickr.com/2391/2153551333_702d874609_b.jpg',
                   id: 2,
-                  linkUrl: 'shop/friedrice'
+                  linkUrl: 'friedrice'
                 },
                 {
                   title: 'Soup',
                   imageUrl: 'https://live.staticflickr.com/8060/8191470839_d33e84c33c_b.jpg',
                   id: 3,
-                  linkUrl: 'shop/soup'
+                  linkUrl: 'soup'
                 },
                 {
                   title: 'Noodles',
                   imageUrl: 'https://live.staticflickr.com/213/521460259_3017c83773_b.jpg',
                   size: 'large',
                   id: 4,
-                  linkUrl: 'shop/noodles'
+                  linkUrl: 'noodles'
                 },
                 {
                   title: 'Seafood',
                   imageUrl: 'https://live.staticflickr.com/4069/4316791101_12de25034e_b.jpg',
                   size: 'large',
                   id: 5,
-                  linkUrl: 'shop/seafood'
+                  linkUrl: 'seafood'
                 }
               ]
         }
@@ -55,10 +55,8 @@ render(){
         <div className='directory-menu'>
             {
                 this.state.sections.map(
-                    ({title, imageUrl, id, size})=>(
-                    <MenuItem key={id} title={title}
-                    imageUrl={imageUrl}
-                    size={size} 
+                    ({id, ...otherSectionProps})=>(
+                    <MenuItem key={id} {...otherSectionProps}
                     />))
             }
         </div>
